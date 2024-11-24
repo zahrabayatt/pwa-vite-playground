7a9cb956d6ee007c2e9737d4a2efea2a066f03f0
Zahra Bayat <zahra.bayat13799@gmail.com>
2024-11-24 18:49:40 +0330

    Install and Config PWA Vite Plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,118 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "pwa vite project",
+        short_name: "PWAViteProject",
+        display: "standalone",
+        background_color: "#FFE9D2",
+        theme_color: "#FFE1C4",
+        orientation: "portrait-primary",
+        icons: [
+          {
+            src: "/img/icons/icon-72x72.png",
+            type: "image/png",
+            sizes: "72x72",
+          },
+          {
+            src: "/img/icons/icon-96x96.png",
+            type: "image/png",
+            sizes: "96x96",
+          },
+          {
+            src: "/img/icons/icon-128x128.png",
+            type: "image/png",
+            sizes: "128x128",
+          },
+          {
+            src: "/img/icons/icon-144x144.png",
+            type: "image/png",
+            sizes: "144x144",
+          },
+          {
+            src: "/img/icons/icon-152x152.png",
+            type: "image/png",
+            sizes: "152x152",
+          },
+          {
+            src: "/img/icons/icon-192x192.png",
+            type: "image/png",
+            sizes: "192x192",
+          },
+          {
+            src: "/img/icons/icon-384x384.png",
+            type: "image/png",
+            sizes: "384x384",
+          },
+          {
+            src: "/img/icons/icon-512x512.png",
+            type: "image/png",
+            sizes: "512x512",
+          },
+        ],
+        screenshots: [
+          {
+            src: "/img/screenshots/screenshot1-0ff68546.png",
+            type: "image/png",
+            sizes: "540x720",
+            form_factor: "narrow",
+          },
+          {
+            src: "/img/screenshots/screenshot2-1f78c4db.jpg",
+            type: "image/jpeg",
+            sizes: "540x720",
+            form_factor: "narrow",
+          },
+          {
+            src: "/img/screenshots/screenshot3-c1e02216.jpg",
+            type: "image/jpeg",
+            sizes: "540x720",
+            form_factor: "narrow",
+          },
+          {
+            src: "/img/screenshots/screenshot4-3a706c3c.png",
+            type: "image/png",
+            sizes: "1024x593",
+            form_factor: "wide",
+          },
+          {
+            src: "/img/screenshots/screenshot5-ea50826f.jpg",
+            type: "image/jpeg",
+            sizes: "1024x593",
+            form_factor: "wide",
+          },
+          {
+            src: "/img/screenshots/screenshot6-0168d284.jpg",
+            type: "image/jpeg",
+            sizes: "1024x593",
+            form_factor: "wide",
+          },
+        ],
+      },
+      registerType: "autoUpdate",
+      devOptions: {
+        enabled: true,
+      },
+      workbox: {
+        runtimeCaching: [
+          {
+            urlPattern: /^https:\/\/fonts\.googleapis\.com\/.*/i,
+            handler: "CacheFirst",
+            options: {
+              cacheName: "api-cache",
+              cacheableResponse: {
+                statuses: [0, 200],
+              },
+            },
+          },
+        ],
+      },
+    }),
+  ],
 });
